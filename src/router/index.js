50600d0a7b78c1548460a90af0228b3f1d60e282
István Ábrahám <istvan.abraham@vicampo.de>
2024-04-29 16:49:09 +0200

    Add navigation guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+let cache
+
 const AccountView = () => import('@/views/AccountView.vue')
 const HomeView = () => import('@/views/HomeView.vue')
 const GroupView = () => import('@/views/GroupView.vue')
+
+const _checkSession = async (_t, _f, next) => {
+  const { user } = await _getDependencies()
+  if (user.isLoggedIn) {
+    next()
+    return
+  }
+
+  next({ name: 'account' })
+}
+
+const _getDependencies = async () => {
+  if (cache) return cache
+
+  const [{ useUserStore }] = await Promise.all([await import('@/stores/user')])
+
+  cache = {
+    user: useUserStore()
+  }
+
+  return cache
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,12 +39,14 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      beforeEnter: [_checkSession]
     },
     {
       path: '/groups/:id',
       name: 'group',
-      component: GroupView
+      component: GroupView,
+      beforeEnter: [_checkSession]
     }
   ]
 })
