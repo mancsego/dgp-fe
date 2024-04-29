b734d039012a42135a6125989244461a04123055
István Ábrahám <istvan.abraham@vicampo.de>
2024-04-29 16:46:40 +0200

    Complete auth functionality

---
+++ src/stores/user.js
@@ -0,0 +1,78 @@
+import { defineStore } from 'pinia'
+
+const TOKEN = 'token'
+
+let cache
+
+export const useUserStore = defineStore('user', {
+  state: () => ({
+    profile: {
+      firstName: '',
+      lastName: '',
+      email: ''
+    },
+    token: {
+      value: null,
+      expires: 0
+    }
+  }),
+  getters: {
+    getProfile: ({ profile }) => profile,
+    getToken: ({ token: { value } }) => value,
+    isLoggedIn: () => sessionStorage.getItem(TOKEN) ?? null
+  },
+  actions: {
+    async register(payload) {
+      const { AUTH, sendPost } = await _getDependencies()
+
+      try {
+        await sendPost(AUTH.REGISTER, payload)
+        await this.login({ email: payload.email, password: payload.password })
+      } catch (_) {
+        alert('boing')
+      }
+    },
+    async login({ email, password }) {
+      const { AUTH, sendPost } = await _getDependencies()
+
+      try {
+        const data = _getTokenFromSession() ?? (await sendPost(AUTH.LOGIN, { email, password }))
+        const token = { value: data.token, expires: Date.now() + data.expiresIn }
+        this.token = token
+        sessionStorage.setItem(TOKEN, JSON.stringify(token))
+      } catch (e) {
+        alert('Boing')
+        console.error(e)
+        await this.logout()
+      }
+    },
+    logout() {
+      sessionStorage.removeItem(TOKEN)
+    },
+    async fetchProfile() {
+      const { USER, sendGet } = await _getDependencies()
+      const { firstName, lastName, email } = await sendGet(USER.PROFILE)
+      this.profile = { firstName, lastName, email }
+    }
+  }
+})
+
+const _getTokenFromSession = () => {
+  const data = JSON.parse(sessionStorage.getItem(TOKEN))
+
+  if (Date.now() > data?.expires ?? 0) return data
+
+  localStorage.removeItem(TOKEN)
+  return null
+}
+
+const _getDependencies = async () => {
+  if (cache) return cache
+
+  const [{ sendPost, sendGet }, { AUTH, USER }] = await Promise.all([
+    await import('@/utilities/RequestHelper'),
+    await import('@/utilities/UrlCollection')
+  ])
+  cache = { sendPost, sendGet, AUTH, USER }
+  return cache
+}
--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { computed, reactive, ref } from 'vue';
+import { useUserStore } from '@/stores/user'
+import { computed, reactive, ref } from 'vue'
 
 const TYPES = {
   SHOW: 'text',
@@ -13,20 +14,24 @@ const ACTIONS = {
 
 const showPw = ref(false)
 const action = ref(ACTIONS.LOGIN)
-
 const state = reactive({
   email: null,
   password: null,
   repeat_password: null,
-  first_name: null,
-  last_name: null
+  firstName: null,
+  lastName: null
 })
+
+const user = useUserStore()
 
 const toggle = () => {
   showPw.value = !showPw.value
 }
 
 const changeAction = () => {
+  Object.keys(state).forEach((key) => {
+    state[key] = null
+  })
   if (action.value === ACTIONS.LOGIN) {
     action.value = ACTIONS.REGISTER
     return
@@ -34,12 +39,19 @@ const changeAction = () => {
   action.value = ACTIONS.LOGIN
 }
 
-const login = () => {
-  console.log('login')
+const login = async () => {
+  const payload = { email: state.email, password: state.password }
+  await user.login(payload)
 }
 
-const register = () => {
-  console.log('register')
+const register = async () => {
+  const payload = {
+    firstName: state.firstName,
+    lastName: state.lastName,
+    email: state.email,
+    password: state.password
+  }
+  await user.register(payload)
 }
 
 const isLogin = computed(() => action.value === ACTIONS.LOGIN)
@@ -48,7 +60,7 @@ const isLogin = computed(() => action.value === ACTIONS.LOGIN)
 <template>
   <div class="flex flex-col justify-center">
     <div class="flex flex-col items-center">
-      <p>{{ isLogin ? 'Don\'t have an account yet?' : 'Already have an account?' }}</p>
+      <p>{{ isLogin ? "Don't have an account yet?" : 'Already have an account?' }}</p>
       <button class="btn" @click="changeAction">
         {{ isLogin ? ACTIONS.REGISTER : ACTIONS.LOGIN }}
       </button>
@@ -58,36 +70,47 @@ const isLogin = computed(() => action.value === ACTIONS.LOGIN)
         <!-- Login -->
         <div v-if="isLogin" class="flex flex-col">
           <label for="email">Email:</label>
-          <input type="text" id="email" name="email" v-model="state.email">
+          <input type="text" id="email" name="email" v-model="state.email" />
           <label for="pwd">Password:</label>
-          <input :type="showPw ? TYPES.SHOW : TYPES.HIDE" id="pwd" name="pwd" v-model="state.password">
+          <input
+            :type="showPw ? TYPES.SHOW : TYPES.HIDE"
+            id="pwd"
+            name="pwd"
+            v-model="state.password"
+          />
           <div>
             <label for="toggle">Show password</label>
             <input id="toggle" type="checkbox" @click="toggle" />
           </div>
-          <button class="btn" @click="login">
-            Login
-          </button>
+          <button class="btn" @click="login">Login</button>
         </div>
         <!-- Register -->
         <div v-else class="flex flex-col">
           <label for="email">Email:</label>
-          <input type="text" id="email" name="email" v-model="state.email">
+          <input type="text" id="email" name="email" v-model="state.email" />
           <label for="first-name">First name:</label>
-          <input type="text" id="first-name" name="first-name" v-model="state.first_name">
+          <input type="text" id="first-name" name="first-name" v-model="state.firstName" />
           <label for="last-name">Last name:</label>
-          <input type="text" id="last-name" name="last-name" v-model="state.last_name">
+          <input type="text" id="last-name" name="last-name" v-model="state.lastName" />
           <label for="pwd">Password:</label>
-          <input :type="showPw ? TYPES.SHOW : TYPES.HIDE" id="pwd" name="pwd" v-model="state.password">
+          <input
+            :type="showPw ? TYPES.SHOW : TYPES.HIDE"
+            id="pwd"
+            name="pwd"
+            v-model="state.password"
+          />
           <label for="re-pwd">Repeat password:</label>
-          <input :type="showPw ? TYPES.SHOW : TYPES.HIDE" id="re-pwd" name="re-pwd" v-model="state.repeat_password">
+          <input
+            :type="showPw ? TYPES.SHOW : TYPES.HIDE"
+            id="re-pwd"
+            name="re-pwd"
+            v-model="state.repeat_password"
+          />
           <div>
             <label for="toggle">Show password</label>
             <input id="toggle" type="checkbox" @click="toggle" />
           </div>
-          <button class="btn" @click="register">
-            Register
-          </button>
+          <button class="btn" @click="register">Register</button>
         </div>
       </div>
     </div>
